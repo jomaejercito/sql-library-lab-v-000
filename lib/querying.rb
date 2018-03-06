436672ff5775b0f4b4aeba6db6a96f95888dd65e
Joma Ejercito <jomaejercito@gmail.com>
2018-03-06 04:22:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,9 +27,18 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+   FROM series
+   JOIN characters ON series.id = characters.series_id
+   GROUP BY series.title
+   ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(*) as book_count
+    FROM character_books
+    INNER JOIN characters
+    ON character_books.character_id = characters.id
+    GROUP BY characters.name
+    ORDER BY book_count DESC;"
 end
